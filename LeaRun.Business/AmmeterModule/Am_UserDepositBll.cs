783a19b67e86ec106774b62d0cf11e9dd5adcbb7
gxlbang <393319165@qq.com>
2018-06-12 17:57:34 +0800

    何201806121757

--- LeaRun.Business/AmmeterModule/Am_UserDepositBll.cs
+++ LeaRun.Business/AmmeterModule/Am_UserDepositBll.cs
@@ -11,11 +11,14 @@
 * ┃            Copyright(c) gxlbang ALL rights reserved                    ┃
 * ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
 */
+using LeaRun.DataAccess;
 using LeaRun.Entity;
 using LeaRun.Repository;
 using LeaRun.Utilities;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Text;
 
 namespace LeaRun.Business
@@ -29,5 +32,97 @@ namespace LeaRun.Business
     /// </summary>
     public class Am_UserDepositBll : RepositoryFactory<Am_UserDeposit>
     {
+        /// <summary>
+        /// 获取列表-导出
+        /// </summary>
+        /// <param name="jqgridparam">分页条件</param>
+        /// <returns></returns>
+        public IList<Am_UserDeposit> GetPageList(ref JqGridParam jqgridparam, string keywords, int Stuts, string StartTime, string EndTime)
+        {
+            StringBuilder strSql = new StringBuilder();
+            List<DbParameter> parameter = new List<DbParameter>();
+            strSql.Append(@"SELECT  *
+                            FROM  Am_UserDeposit where 1=1 ");
+            //状态
+            if (Stuts >= 0)
+            {
+                strSql.Append(" AND Status = @Stuts");
+                parameter.Add(DbFactory.CreateDbParameter("@Stuts", Stuts));
+            }
+            //关键字
+            if (!string.IsNullOrEmpty(keywords))
+            {
+                strSql.Append(@" AND (UserName LIKE @keyword 
+                                    OR U_Name LIKE @keyword 
+                                     OR Province LIKE @keyword 
+                                     OR City LIKE @keyword 
+                                     OR County LIKE @keyword 
+                                     OR Address LIKE @keyword 
+                                     OR Cell LIKE @keyword 
+                                     OR Floor LIKE @keyword 
+                                     OR Room LIKE @keyword 
+                                    OR Ammeter_Code LIKE @keyword)");
+                parameter.Add(DbFactory.CreateDbParameter("@keyword", '%' + keywords + '%'));
+            }
+            //开始时间
+            if (!string.IsNullOrEmpty(StartTime))
+            {
+                strSql.Append(" AND CreateTime > @StartTime");
+                parameter.Add(DbFactory.CreateDbParameter("@StartTime", Convert.ToDateTime(StartTime).ToString("yyyy-MM-dd") + " 00:00:00"));
+            }
+            //结束时间
+            if (!string.IsNullOrEmpty(EndTime))
+            {
+                strSql.Append(" AND CreateTime < @EndTime");
+                parameter.Add(DbFactory.CreateDbParameter("@EndTime", Convert.ToDateTime(EndTime).AddDays(1).ToString("yyyy-MM-dd") + " 00:00:00"));
+            }
+            return Repository().FindListPageBySql(strSql.ToString(), parameter.ToArray(), ref jqgridparam);
+        }
+        /// <summary>
+        /// 获取列表-导出
+        /// </summary>
+        /// <param name="jqgridparam">分页条件</param>
+        /// <returns></returns>
+        public IList<Am_UserDeposit> GetPageList(string keywords, int Stuts, string StartTime, string EndTime)
+        {
+            StringBuilder strSql = new StringBuilder();
+            List<DbParameter> parameter = new List<DbParameter>();
+            strSql.Append(@"SELECT  *
+                            FROM  Am_UserDeposit where 1=1 ");
+            //状态
+            if (Stuts >= 0)
+            {
+                strSql.Append(" AND Status = @Stuts");
+                parameter.Add(DbFactory.CreateDbParameter("@Stuts", Stuts));
+            }
+            //关键字
+            if (!string.IsNullOrEmpty(keywords))
+            {
+                strSql.Append(@" AND (UserName LIKE @keyword 
+                                    OR U_Name LIKE @keyword 
+                                     OR Province LIKE @keyword 
+                                     OR City LIKE @keyword 
+                                     OR County LIKE @keyword 
+                                     OR Address LIKE @keyword 
+                                     OR Cell LIKE @keyword 
+                                     OR Floor LIKE @keyword 
+                                     OR Room LIKE @keyword 
+                                    OR Ammeter_Code LIKE @keyword)");
+                parameter.Add(DbFactory.CreateDbParameter("@keyword", '%' + keywords + '%'));
+            }
+            //开始时间
+            if (!string.IsNullOrEmpty(StartTime))
+            {
+                strSql.Append(" AND CreateTime > @StartTime");
+                parameter.Add(DbFactory.CreateDbParameter("@StartTime", Convert.ToDateTime(StartTime).ToString("yyyy-MM-dd") + " 00:00:00"));
+            }
+            //结束时间
+            if (!string.IsNullOrEmpty(EndTime))
+            {
+                strSql.Append(" AND CreateTime < @EndTime");
+                parameter.Add(DbFactory.CreateDbParameter("@EndTime", Convert.ToDateTime(EndTime).AddDays(1).ToString("yyyy-MM-dd") + " 00:00:00"));
+            }
+            return Repository().FindListBySql(strSql.ToString(), parameter.ToArray());
+        }
     }
 }
--- LeaRun.WebApp/Areas/AmmeterModule/Controllers/Am_UserDepositController.cs
+++ LeaRun.WebApp/Areas/AmmeterModule/Controllers/Am_UserDepositController.cs
@@ -18,6 +18,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -29,5 +30,65 @@ namespace LeaRun.WebApp.Areas.AmmeterModule.Controllers
     /// </summary>
     public class Am_UserDepositController : PublicController<Am_UserDeposit>
     {
+        /// <summary>
+        /// 搜索
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult GridPageListJson(JqGridParam jqgridparam, string keywords, int Stuts, string StartTime, string EndTime)
+        {
+            try
+            {
+                Stopwatch watch = CommonHelper.TimerStart();
+                Am_UserDepositBll bll = new Am_UserDepositBll();
+                var ListData = bll.GetPageList(ref jqgridparam, keywords, Stuts, StartTime, EndTime);
+                var JsonData = new
+                {
+                    total = jqgridparam.total,
+                    page = jqgridparam.page,
+                    records = jqgridparam.records,
+                    costtime = CommonHelper.TimerEnd(watch),
+                    rows = ListData
+                };
+                return Content(JsonData.ToJson());
+            }
+            catch (Exception ex)
+            {
+                Base_SysLogBll.Instance.WriteLog("", OperationType.Query, "-1", "异常错误：" + ex.Message);
+                return null;
+            }
+        }
+        /// <summary>
+        /// 数据导出
+        /// </summary>
+        public void ExportExcel(string keywords, int Stuts, string StartTime, string EndTime)
+        {
+            Am_UserDepositBll bll = new Am_UserDepositBll();
+            var ListData = bll.GetPageList(keywords, Stuts, StartTime, EndTime);
+            var newlist = new List<Am_UserDepositNew>();
+            foreach (var item in ListData)
+            {
+                var model = new Am_UserDepositNew();
+                model.Address = item.Address;
+                model.Ammeter_Code = item.Ammeter_Code;
+                model.Cell = item.Cell;
+                model.City = item.City;
+                model.County = item.County;
+                model.Floor = item.Floor;
+                model.Province = item.Province;
+                model.Room = item.Room;
+                model.CreateTime = item.CreateTime.Value.ToString("yyyy-MM-dd HH:mm:ss");
+                model.Money = item.Money.Value.ToString("0.00");
+                model.StatusStr = item.StatusStr;
+                model.UserName = item.UserName;
+                model.U_Name = item.U_Name;
+
+                newlist.Add(model);
+            }
+            string[] columns = new string[] { "用户名:UserName", "姓名:U_Name", "电表编号:Ammeter_Code",
+                "省:Province","市:City","区县:County","地址:Address","单元:Cell","楼层:Floor",
+                "房间:Room", "时间:CreateTime","状态:StatusStr","押金:Money" };
+            DeriveExcel.ListToExcel<Am_UserDepositNew>(newlist, columns, "押金信息表" + DateTime.Now.ToString("yyyyMMddHHmmss"));
+
+        }
     }
 }
